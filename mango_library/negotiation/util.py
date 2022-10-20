adffcac3295a5e4bfb53cf4569e60e8ea32afb44
m.nebel-wenner <mnw@offis.de>
2022-10-20 13:33:30 +0200

    Remove multi objective stuff

--- mango_library/negotiation/util.py
+++ mango_library/negotiation/util.py
@@ -6,9 +6,6 @@ from mango_library.coalition.core import CoalitionInvite, CoaltitionResponse, Co
 from mango_library.negotiation.cohda.data_classes import ScheduleSelection, \
     SystemConfig, SolutionCandidate, WorkingMemory
 
-from mango_library.negotiation.multiobjective_cohda.data_classes import ScheduleSelections,\
-    SystemConfig as SystemConfig_m, SolutionCandidate as SolutionCandidate_m, WorkingMemory as WorkingMemory_m
-from mango_library.negotiation.multiobjective_cohda.multiobjective_cohda import CohdaMessage as CohdaMessage_m
 from mango_library.negotiation.cohda.cohda import CohdaMessage
 from mango_library.negotiation.core import NegotiationMessage
 from mango_library.negotiation.termination import TerminationMessage
@@ -70,23 +67,6 @@ cohda_serializers = [
     SolutionCandidate.__serializer__,
     WorkingMemory.__serializer__,
     CohdaMessage.__serializer__,
-    NegotiationMessage.__serializer__,
-    TerminationMessage.__serializer__,
-    get_fraction_serializer,
-]
-
-
-multi_objective_serializers = [
-    get_np_serializer,
-    get_uuid_serializer,
-    CoalitionInvite.__serializer__,
-    CoaltitionResponse.__serializer__,
-    CoalitionAssignment.__serializer__,
-    ScheduleSelections.__serializer__,
-    SystemConfig_m.__serializer__,
-    SolutionCandidate_m.__serializer__,
-    WorkingMemory_m.__serializer__,
-    CohdaMessage_m.__serializer__,
     NegotiationMessage.__serializer__,
     TerminationMessage.__serializer__,
     get_fraction_serializer,
